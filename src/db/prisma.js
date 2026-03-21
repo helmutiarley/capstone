834c0490d92ae6ba8ff47e4e7460a90d8699e82f
Helmut Iarley <helmut.silva@hostinger.com>
2026-03-20 22:34:34 -0300

    fix prisma import for docker runtime

--- src/db/prisma.js
+++ src/db/prisma.js
@@ -1,8 +1,7 @@
 import prismaClientPackage from "@prisma/client";
-import prismaPgPackage from "@prisma/adapter-pg";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 const { PrismaClient } = prismaClientPackage;
-const { PrismaPg } = prismaPgPackage;
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is not set");
